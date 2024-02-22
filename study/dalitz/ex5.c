b0fadf4aa9552a480110a67c3274a092c1d8c9f8
seafontes <alchamiwa@gmail.com>
2024-02-22 12:06:33 -0300

    Thu Feb 22 12:06:33 -03 2024

--- study/dalitz/ex5.c
+++ study/dalitz/ex5.c
@@ -18,19 +18,20 @@ double s13_bound(double m1, double m2, double m3, double m0, double s12, double 
 	double s = m0*m0;
 	double lbds = -1.0*(uplow)*lambdasqrt(s12, m2*m2, m1*m1)*lambdasqrt(s,s12,m3*m3);
 	double brkt = (s12-s+m3*m3)*(s12+m1*m1-m2*m2)+lbds;
-	return m1*m1 + m3*m3 -(1./2*s12)*brkt;
+	return m1*m1 + m3*m3 -(1./(2.*s12))*brkt;
 
 }
 
 
 
 void ex5(){
+
 	TRandom3 randomGenerator(0);
 	
 	double s12_min, s12_max, s13_min, s13_max;
 	double m1, m2, m3, m0;
-	int i;
-	double randoms12, randoms13;
+	int i=0;
+	double s12, s13;
 	
 	
 	
@@ -38,29 +39,26 @@ void ex5(){
 	m0=massD;	m1=massK;	m2=massK;	m3=massPi;
 	
 	//----DEFINICAO DOS LIMITES BRUTOS
-	s12_min=(m1+m2)*(m1+m2);
-	s12_max=(m0-m3)*(m0-m3);
-	s13_min=(m1+m3)*(m1+m3);
-	s13_max=(m0-m2)*(m0-m2);
+	s12_min=(m1+m2)*(m1+m2);	s12_max=(m0-m3)*(m0-m3);
+	
+	s13_min=(m1+m3)*(m1+m3);	s13_max=(m0-m2)*(m0-m2);
+	
 	
 	TH2F *hist = new TH2F("hist", "hist", 1000, s12_min, s12_max, 1000, s13_min, s13_max);
+
 	
-	
-	i=0;
-	while(i<10000){
+	while(i<100000){
 		
-		cout << i <<endl;
+		s12 = s12_min+(s12_max-s12_min)*randomGenerator.Rndm();
+		s13 = s13_min+(s13_max-s13_min)*randomGenerator.Rndm();
 		
-		randoms12 = s12_min+(s12_max-s12_min)*randomGenerator.Rndm();
-		randoms13 = s13_min+(s13_max-s13_min)*randomGenerator.Rndm();
+		double boundup 	= s13_bound(m1,m2,m3,m0,s12,1.0);
+		double boundlow = s13_bound(m1,m2,m3,m0,s12,-1.0);
 		
-		if((randoms13>=s13_bound(m1,m2,m3,m0,randoms12,-1.))&&(randoms13<=s13_bound(m1,m2,m3,m0,randoms12,1.0))){
-			
-			hist->Fill(randoms12,randoms13);
+		if((s13>boundlow)&&(s13<boundup)){
+			hist->Fill(s12,s13);
 			i++;
-			
-		}	
-	
+		}
 	}	
 	
 	TCanvas *c1 = new TCanvas();
